c3fcee2bd80fe2d988deaa628c05832c487a7200
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-08-29 01:16:03 -0700

    Update bridge-blink.py

--- bridge-blink.py
+++ bridge-blink.py
@@ -3,11 +3,11 @@
 
 from gpiozero import LED
 import time
-              #    Motor term.      Battery side     Wire to    Relay ID  Transistor  GPIO
-T2=LED(17)    #    Left armature    Positive side    X3-2       K1        T2          17
-T3=LED(22)    #    Left armature    Negative side    X3-3       K2        T3          22
-T4=LED(27)    #    Right armature   Positive side    X3-4       K3        T4          27
-T5=LED(23)    #    Right armature   Negative side    X3-5       K4        T5          23
+              #    Motor term.      Battery side     Wire to    Relay ID  Transistor  GPIO    Header pin
+T2=LED(17)    #    Left armature    Positive side    X3-2       K1        T2          17      11
+T3=LED(22)    #    Left armature    Negative side    X3-3       K2        T3          22      15
+T4=LED(27)    #    Right armature   Positive side    X3-4       K3        T4          27      13
+T5=LED(23)    #    Right armature   Negative side    X3-5       K4        T5          23      16
 led=LED(18)   #    LED-10, driven by GPIO-18
 led.off()
 T2.off()
