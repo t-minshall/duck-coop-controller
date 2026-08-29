c3a3d790b06b8ff057267c3208c9c2c4aef74ec3
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-08-29 03:26:45 -0700

    Update read_inputs.py

--- read_inputs.py
+++ read_inputs.py
@@ -62,7 +62,15 @@ def Flash_LED_latch_sw():
         time.sleep(0.2)
         led.off()
         time.sleep(0.2)
-    
+
+def announce_input(code,button_name);
+    printf("Button "+code+"pressed: "+button_name
+    for _ in range(code):
+        led.on()
+        time.sleep(0.2)
+        led.off()
+        time.sleep(0.2)
+
 #Define all IO ports
 led=LED(18)
 buzzer=LED(25)
@@ -88,13 +96,21 @@ print("Closed u-switch      GPIO-26    Header-37      5 flashes    ")
 print("Torque u-switch      GPIO-20    Header-38      6 flashes    ")
 print("Latch disable sw     GPIO-21    Header-40      7 flashes    ")
 while True:
-    btn_open.when_pressed = Flash_LED_open_cmd
-    btn_close.when_pressed = Flash_LED_close_cmd
-    btn_stop.when_pressed = Flash_LED_stop_cmd
-    sw_open.when_pressed = Flash_LED_open_sw
-    sw_close.when_pressed = Flash_LED_closed_sw
-    sw_torque.when_pressed = Flash_LED_torque_sw
-    sw_latch.when_pressed = Flash_LED_latch_sw
+    while btn_open.is_pressed:
+        announce_input(1,"Open Command")
+    while sw_torque.is_pressed:
+        announce_input(6, "Over-torque switch")
+    while sw_latch.is_pressed:
+        announce_input(7, "Latch overide switch")
+                           
+        
+#    btn_open.when_pressed = Flash_LED_open_cmd
+#    btn_close.when_pressed = Flash_LED_close_cmd
+#    btn_stop.when_pressed = Flash_LED_stop_cmd
+#    sw_open.when_pressed = Flash_LED_open_sw
+#    sw_close.when_pressed = Flash_LED_closed_sw
+#    sw_torque.when_pressed = Flash_LED_torque_sw
+#    sw_latch.when_pressed = Flash_LED_latch_sw
 
 #hang program, let "when_pressed" interrupts execute
 #not sure how to debounce
