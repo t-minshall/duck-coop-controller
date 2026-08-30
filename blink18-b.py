5e8ca5716234133c421664fbefdc6fad214089eb
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-08-29 22:29:52 -0700

    Update blink18-b.py

--- blink18-b.py
+++ blink18-b.py
@@ -1,5 +1,9 @@
 from gpiozero import LED
 import time
+from signal import pause
 led10 = LED(18)
-while True:
-  led10.blink()
+#while True:
+#  led10.blink(on_time=0.2, off_time=0.4)
+
+led10.blink(on_time=0.2, off_time=0.4)
+pause()
