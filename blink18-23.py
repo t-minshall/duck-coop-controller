00b72c7536dcddb9c8ac1ef5635a0dd05b87b300
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-08-29 21:54:52 -0700

    Update blink18-23.py

--- blink18-23.py
+++ blink18-23.py
@@ -3,12 +3,12 @@ import time
 T5=LED(23)
 LED10=LED(18)
 
-for _ in range(15):
+for _ in range(5):
     LED10.on()
     T5.off()
     print("light on,  transistor off")
-    time.sleep(0.4)
+    time.sleep(1.4)
     LED10.off()
     T5.on()
     print("light off, transistor on")
-    time.sleep(0.4)
+    time.sleep(1.4)
