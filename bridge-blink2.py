ea10b3e7d06c49c3a99da2870b9935bd7ff32f23
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-09-02 02:42:23 -0700

    Update bridge-blink2.py

--- bridge-blink2.py
+++ bridge-blink2.py
@@ -1,2 +1,17 @@
 import init_controller
-init_controller.motor_loop
+'init_controller.motor_loop
+
+#def motor_loop():
+#  while True: 
+#    motor_fwd()
+#    time.sleep(1)
+#    motor_bkwd()
+#    time.sleep(1)
+#  return
+
+init_controller.motor_off()
+while True:
+  init_controller.motor_fwd()
+  time.sleep(1)
+  init_controller.motor_bkwd()
+  time.sleeep(1)
