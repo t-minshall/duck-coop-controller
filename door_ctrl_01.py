d3ba3b569d6243d83c5ca6f19880da1739fab7c6
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-09-10 01:05:10 -0700

    Update door_ctrl_01.py

--- door_ctrl_01.py
+++ door_ctrl_01.py
@@ -1,34 +1,34 @@
-def initialize():
-    from gpiozero import LED
-    from gpiozero import Button
-    import time
-    LED10=LED(18)  # General purpose annunciator (blue)
-    T5=LED(23)   # Motor x y (red)
-    T4=LED(27)   # Motor x y (red)
-    T3=LED(22)   # Motor x y (red)
-    T2=LED(17)   # Motor x y (red)
-    #T1=LED(25)   # Buzzer (greeen)
-    Buzzer=LED(25) # Buzzer (green)
-    #T6=LED(7)    # Hard Latch (green)  ******* GPIO-7 seems broken on my Pi ... re-routing to GPIO-12
-    T6=LED(12)    # Hard Latch (green)
-    T7=LED(8)    # Soft Latch (green)
-    CMD_latch=Button(21, bounce_time=0.05)
-    CMD_open=Button(5, bounce_time=0.05)
-    CMD_close=Button(6, bounce_time=0.05)
-    CMD_stop=Button(13, bounce_time=0.05)
-    SW_open=Button(19, bounce_time=0.05)
-    SW_closed=Button(26, bounce_time=0.05)
-    SW_torque=Button(20, bounce_time=0.05)
-    jog_delay=0.25                                #    Time when motor can be force-driven under over-torque condition
-    open_time=10                                  #    Time it should take to fully open the door
-    close_sns_time=1                              #    Time it should take to toggle the open-sensor from a normal-closed state
-    close_time=10                                 #    Time it should take to fully close the door
-    open_sns_time=1                               #    Time it should take to toggle the open-sensor from a normal-open state
-    armature_debounce=0.2                        #    Time delay before energizing motor armature relays (prevent shorting)
-initialize()
+#Initialization:
+from gpiozero import LED
+from gpiozero import Button
+import time
+LED10=LED(18)  # General purpose annunciator (blue)
+T5=LED(23)   # Motor x y (red)
+T4=LED(27)   # Motor x y (red)
+T3=LED(22)   # Motor x y (red)
+T2=LED(17)   # Motor x y (red)
+#T1=LED(25)   # Buzzer (greeen)
+Buzzer=LED(25) # Buzzer (green)
+#T6=LED(7)    # Hard Latch (green)  ******* GPIO-7 seems broken on my Pi ... re-routing to GPIO-12
+T6=LED(12)    # Hard Latch (green)
+T7=LED(8)    # Soft Latch (green)
+CMD_latch=Button(21, bounce_time=0.05)
+CMD_open=Button(5, bounce_time=0.05)
+CMD_close=Button(6, bounce_time=0.05)
+CMD_stop=Button(13, bounce_time=0.05)
+SW_open=Button(19, bounce_time=0.05)
+SW_closed=Button(26, bounce_time=0.05)
+SW_torque=Button(20, bounce_time=0.05)
+jog_delay=0.25                                #    Time when motor can be force-driven under over-torque condition
+open_time=10                                  #    Time it should take to fully open the door
+close_sns_time=1                              #    Time it should take to toggle the open-sensor from a normal-closed state
+close_time=10                                 #    Time it should take to fully close the door
+open_sns_time=1                               #    Time it should take to toggle the open-sensor from a normal-open state
+armature_debounce=0.2                        #    Time delay before energizing motor armature relays (prevent shorting)
+
 
 def stop_all():
-    #T2.off()
+    T2.off()
     T3.off()
     T4.off()
     T5.off()
