03578ecb4613aebc5b9b6e9c081aaaafe19c3e0a
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-09-09 23:41:37 -0700

    Create door_ctrl_01.py

---
+++ door_ctrl_01.py
@@ -0,0 +1,24 @@
+initialize()
+var_a = 1
+print("running -001")
+
+def initialize:
+    from gpiozero import LED
+    from gpiozero import Button
+    import time
+    LED10=LED(18)  # General purpose annunciator (blue)
+    T5=LED(23)   # Motor x y (red)
+    T4=LED(27)   # Motor x y (red)
+    T3=LED(22)   # Motor x y (red)
+    T2=LED(17)   # Motor x y (red)
+    T1=LED(25)   # Buzzer (greeen)
+    #T6=LED(7)    # Hard Latch (green)  ******* GPIO-7 seems broken on my Pi ... re-routing to GPIO-12
+    T6=LED(12)    # Hard Latch (green)
+    T7=LED(8)    # Soft Latch (green)
+    CMD_latch=Button(21, bounce_time=0.05)
+    CMD_open=Button(5, bounce_time=0.05)
+    CMD_close=Button(6, bounce_time=0.05)
+    CMD_stop=Button(13, bounce_time=0.05)
+    SW_open=Button(19, bounce_time=0.05)
+    SW_closed=Button(26, bounce_time=0.05)
+    SW_torque=Button(20, bounce_time=0.05)
