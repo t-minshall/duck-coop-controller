cd40c1ffc196c4dfe64b0b8225c8dd415c1a6363
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-09-13 05:24:13 -0700

    Update door-ctrl-02.py

--- door-ctrl-02.py
+++ door-ctrl-02.py
@@ -9,11 +9,11 @@ section=9
 def LED_flash(numbr):
     print(f"Background task to flash {numbr} started...")
     time.sleep(1)
-    print(f"Background task {numbr} finished!")
+    print(f"Background task (flash-{numbr}) finished!")
 
 # Create the thread
 # Use daemon=True so the background task stops automatically if the main program exits
-thread = threading.Thread(target=LED_flash, args=(4), daemon=True)
+thread = threading.Thread(target=LED_flash, args=("1"), daemon=True)
 
 # Start the background task
 thread.start()
