55601c7767c281eba7909cb5b71f965f721bfc67
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-09-13 04:29:14 -0700

    Update door-ctrl-02.py

--- door-ctrl-02.py
+++ door-ctrl-02.py
@@ -51,7 +51,8 @@ if section==9:
         message="looping"
         sys_state_local=init_controller.system_state()
         print(f"{message:<50} {sys_state_local} {direction} 1", end=print_end)
-        if not sys_state_local==sys_state_old:
+        #if not sys_state_local==sys_state_old:
+        if not sys_state_local[-18]==sys_state_old[-18]:
             logging.info(sys_state_local+" | "+message)
             sys_state_old=sys_state_local
         
