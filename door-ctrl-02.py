1704ca1a8a22163c0f13df7bbc5bb614f2d1caff
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-09-13 03:25:54 -0700

    Update door-ctrl-02.py

--- door-ctrl-02.py
+++ door-ctrl-02.py
@@ -44,7 +44,7 @@ if section==9:
     message="NO buttons pressed, stopping motor"
     print(f"{message:<50} {sys_state_local} {direction} 0", end=print_end)
     #print(f"{message:<50} {sys_state_local}", end=print_end)
-    logging.info(message,sys_state_local)
+    logging.info(message+sys_state_local)
     while True:
         message="looping"
         sys_state_local=init_controller.system_state()
