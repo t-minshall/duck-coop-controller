aa3376457d310830ed15db8724489538edc1b958
t-minshall <48506851+t-minshall@users.noreply.github.com>
2026-09-13 03:50:04 -0700

    Update door-ctrl-02.py

--- door-ctrl-02.py
+++ door-ctrl-02.py
@@ -42,6 +42,7 @@ if section==9:
     init_controller.T7.off()
     init_controller.LED10.blink(0.2, 1.8)
     sys_state_local=init_controller.system_state()
+    sys_state_old=sys_state_local
     message="NO buttons pressed, stopping motor"
     print(f"{message:<50} {sys_state_local} {direction} 0", end=print_end)
     #print(f"{message:<50} {sys_state_local}", end=print_end)
@@ -50,7 +51,9 @@ if section==9:
         message="looping"
         sys_state_local=init_controller.system_state()
         print(f"{message:<50} {sys_state_local} {direction} 1", end=print_end)
-        #logging.info(message,sys_state_local)
+        if not sys_state_local==sys_state_old
+            logging.info(message,sys_state_local)
+            sys_state_old=sys_state_local
         
         while init_controller.CMD_open.is_pressed:
             if direction!="OPEN":
@@ -67,7 +70,9 @@ if section==9:
             sys_state_local=init_controller.system_state()
             message="Button OPEN is pressed, Motor turning FWD/OPEN"
             print(f"{message:<50} {sys_state_local} {direction} 2", end=print_end)
-            logging.info(sys_state_local+" | "+message)
+            if not sys_state_local==sys_state_old
+                logging.info(message,sys_state_local)
+                sys_state_old=sys_state_local
             
         if direction=="OPEN" and init_controller.SW_open.is_pressed:
             # motor has moved all the way to the open position-sensor
@@ -81,7 +86,9 @@ if section==9:
             message="Door had fully opened, Stop Motors"
             #print("Door had fully opened, Stop Motors",end=print_end)
             print(f"{message:<50} {sys_state_local} {direction} 3", end=print_end)
-            logging.info(sys_state_local+" | "+message)
+            if not sys_state_local==sys_state_old
+                logging.info(message,sys_state_local)
+                sys_state_old=sys_state_local
         
         while init_controller.CMD_close.is_pressed:
             if direction!="CLOSE":
@@ -96,7 +103,9 @@ if section==9:
             sys_state_local=init_controller.system_state()
             message="Button CLOSE is pressed, Motor turning REV/CLOSE"
             print(f"{message:<50} {sys_state_local} {direction} 4", end=print_end)
-            logging.info(sys_state_local+" | "+message)
+            if not sys_state_local==sys_state_old
+                logging.info(message,sys_state_local)
+                sys_state_old=sys_state_local
 
         if direction=="CLOSE" and init_controller.SW_closed.is_pressed:
             # motor has moved all the way to the closed position-sensor
@@ -115,7 +124,9 @@ if section==9:
             #sys_state_local=init_controller.system_state()
             message="Door had fully closed, Stop Motors, Set Latch"
             print(f"{message:<50} {sys_state_local} {direction} 5", end=print_end)
-            logging.info(sys_state_local+" | "+message)
+            if not sys_state_local==sys_state_old
+                logging.info(message,sys_state_local)
+                sys_state_old=sys_state_locallogging.info(sys_state_local+" | "+message)
             
         while init_controller.CMD_stop.is_pressed:
             init_controller.T1.off()
@@ -130,7 +141,9 @@ if section==9:
             sys_state_local=init_controller.system_state()
             message="Button STOP is pressed, Motor halted"
             print(f"{message:<50} {sys_state_local} {direction} 6", end=print_end)
-            logging.info(sys_state_local+" | "+message)
+            if not sys_state_local==sys_state_old
+                logging.info(message,sys_state_local)
+                sys_state_old=sys_state_locallogging.info(sys_state_local+" | "+message)
             
         if init_controller.SW_torque.is_pressed:
             # Note: system will get hung-up here ... no way to jog-out while over-torque is sensed
@@ -146,5 +159,7 @@ if section==9:
             sys_state_local=init_controller.system_state()
             message="Over-Torque detected, Stop motors, sound alarm"
             print(f"{message:<50} {sys_state_local} {direction} 7", end=print_end)
-            logging.info(sys_state_local+" | "+message)
+            if not sys_state_local==sys_state_old
+                logging.info(message,sys_state_local)
+                sys_state_old=sys_state_locallogging.info(sys_state_local+" | "+message)
           
